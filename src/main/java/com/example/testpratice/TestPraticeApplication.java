f1b0a7bf0766dd4597898a2cb352e08f0b05a52c
jeongheekim <8055kjh@gmail.com>
2023-08-04 17:05:23 +0900

    Initial commit

---
+++ src/main/java/com/example/testpratice/TestPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.testpratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPraticeApplication.class, args);
+    }
+
+}
